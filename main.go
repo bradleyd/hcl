43dd5c5754a56af10e50815e40be5f45c0ea2331
Bradley Smith <brad.smith@sendgrid.com>
2015-04-10 09:27:18 -0600

    leaving SG commit

--- main.go
+++ main.go
@@ -1,59 +1,90 @@
 package main
 
 import (
-  "github.com/codegangsta/cli"
-  "os"
-  "hipchat"
+	"github.com/codegangsta/cli"
+	"hipchat"
+	"os"
 )
 
 func main() {
-  tken := os.Getenv("TOKEN")
-  hc := hipchat.New(tken)
+	tken := os.Getenv("TOKEN")
+	hc := hipchat.New(tken)
 
-  app := cli.NewApp()
-  app.Usage = "Hipchat command line application"
-  app.Version = "0.0.1"
-  app.Commands = []cli.Command{
-    {
-      Name:  "room",
-      ShortName: "r",
-      Usage: "Rooms",
-      Subcommands: []cli.Command{
-        {
-          Name:  "message",
-          Usage: "send a message to a room",
-          Action: func(c *cli.Context) {
-            if len(c.Args()) < 2 {
-              println("room name and message are required.")
-              os.Exit(1)
-            }
+	app := cli.NewApp()
+	app.Usage = "Hipchat command line application"
+	app.Version = "0.0.1"
+	app.Commands = []cli.Command{
+		{
+			Name:      "user",
+			ShortName: "u",
+			Usage:     "Users",
+			Subcommands: []cli.Command{
+				{
+					Name:  "message",
+					Usage: "send a message to a user",
+					Action: func(c *cli.Context) {
+						if len(c.Args()) < 2 {
+							println("user name and message are required.")
+							os.Exit(1)
+						}
 
-            room := c.Args().First()
-            msg := c.Args()[1]
-            from := os.Getenv("USER")
-            res, err := hc.MessageRoom(room, msg, from)
-            if err != nil {
-              println(err)
-              os.Exit(1)
-            }
+						user := c.Args().First()
+						msg := c.Args()[1]
+						from := os.Getenv("USER")
+						res, err := hc.MessageUser(user, msg, from)
+						if err != nil {
+							println(err)
+							os.Exit(1)
+						}
 
-            if res != "204" {
-              println("Message sent")
-            }else {
-              println("RoomMessageError: ", res)
-            }
-          },
-        },
-        {
-          Name:  "list",
-          Usage: "List rooms",
-          Action: func(c *cli.Context) {
-            println("list rooms: ", c.Args().First())
-          },
-        },
+						if res != "204" {
+							println("Message sent")
+						} else {
+							println("RoomMessageError: ", res)
+						}
+					},
+				},
+			},
+		},
+		{
+			Name:      "room",
+			ShortName: "r",
+			Usage:     "Rooms",
+			Subcommands: []cli.Command{
+				{
+					Name:  "message",
+					Usage: "send a message to a room",
+					Action: func(c *cli.Context) {
+						if len(c.Args()) < 2 {
+							println("room name and message are required.")
+							os.Exit(1)
+						}
 
-      },
-    },
-  }
-  app.Run(os.Args)
+						room := c.Args().First()
+						msg := c.Args()[1]
+						from := os.Getenv("USER")
+						res, err := hc.MessageRoom(room, msg, from)
+						if err != nil {
+							println(err)
+							os.Exit(1)
+						}
+
+						if res != "204" {
+							println("Message sent")
+						} else {
+							println("RoomMessageError: ", res)
+						}
+					},
+				},
+				{
+					Name:  "list",
+					Usage: "List rooms",
+					Action: func(c *cli.Context) {
+						println("list rooms: ", c.Args().First())
+					},
+				},
+			},
+		},
+	}
+	app.Run(os.Args)
 }
--- src/hipchat/hipchat.go
+++ src/hipchat/hipchat.go
@@ -1,26 +1,26 @@
 package hipchat
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-        "bytes"
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "bytes"
 )
 
 type Room struct {
-	Room_id int    `json:"room_id"`
-	Name    string `json:"name"`
-        Topic   string `json:"topic"`
+  Room_id int    `json:"room_id"`
+  Name    string `json:"name"`
+  Topic   string `json:"topic"`
 }
 
 type Hipchat struct {
-	token string
-	url   string
+  token string
+  url   string
 }
 
 type HipchatFeed struct {
-	Rooms []Room `json:"rooms"`
+  Rooms []Room `json:"rooms"`
 }
 
 type MessageResponse struct {
@@ -33,92 +33,163 @@ type MessagePayload struct {
 }
 
 func New(token string) *Hipchat {
-	return &Hipchat{token, "https://api.hipchat.com/v2"}
+  return &Hipchat{token, "https://api.hipchat.com/v2"}
 }
 
+//func request(method string, url string, data []byte) ([]byte, error) {
+  //var response []byte
+  //h := &http.Client{}
+
+  //body := bytes.NewBuffer(data)
+  //req, err := http.NewRequest(method, url, body)
+  //if err != nil {
+    //return response, err
+  //}
+
+   ////for v2 only
+  //authHeader := fmt.Sprintf("Bearer %s", token)
+  //req.Header.Add("Authorization", authHeader)
+  //req.Header.Add("Accept", "application/json")
+  //req.Header.Add("Content-Type", "application/json")
+  
+  //res := &http.Response{}
+
+
+  //defer res.Body.Close()
+
+  //return response, err
+//}
 // post a message to a room
 // todo create messagePayload from config
 // takes POST url, room name, message, and auth token
 func postMessage(path, room, message, token string) (string, error) {
-        // create payload 
-        json_data := &MessagePayload {
-          Color: "green",
-          Message: message,
-        }
-        json_bytes, err := json.Marshal(json_data)
-        if nil != err {
-          return "JSON marshall error", err
-        }
+  // create payload 
+  json_data := &MessagePayload {
+    Color: "green",
+    Message: message,
+  }
+  json_bytes, err := json.Marshal(json_data)
+  if nil != err {
+    return "JSON marshall error", err
+  }
 
-	req, err := http.NewRequest("POST", path, bytes.NewReader(json_bytes))
-	if err != nil {
-		fmt.Println("postMessage ERROR: ", err)
-	}
+  req, err := http.NewRequest("POST", path, bytes.NewReader(json_bytes))
+  if err != nil {
+    fmt.Println("postMessage ERROR: ", err)
+  }
 
-        // for v2 only
-        authHeader := fmt.Sprintf("Bearer %s", token)
-        req.Header.Add("Authorization", authHeader)
-        req.Header.Add("Accept", "application/json")
-        req.Header.Add("Content-Type", "application/json")
+  // for v2 only
+  authHeader := fmt.Sprintf("Bearer %s", token)
+  req.Header.Add("Authorization", authHeader)
+  req.Header.Add("Accept", "application/json")
+  req.Header.Add("Content-Type", "application/json")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("client do ERROR: ", err)
-	}
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    fmt.Println("client do ERROR: ", err)
+  }
 
-	defer resp.Body.Close()
+  defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Read body ERROR: ", err)
-	}
-        fmt.Println(body)
-	return resp.Status, nil
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Read body ERROR: ", err)
+  }
+  fmt.Println(body)
+  return resp.Status, nil
+}
+
+// post a message to a user
+// todo create messagePayload from config
+// takes POST url, room name, message, and auth token
+func postUserMessage(path, user, message, token string) (string, error) {
+  // create payload 
+  json_data := &MessagePayload {
+    Color: "green",
+    Message: message,
+  }
+  json_bytes, err := json.Marshal(json_data)
+  if nil != err {
+    return "JSON marshall error", err
+  }
+
+  req, err := http.NewRequest("POST", path, bytes.NewReader(json_bytes))
+  if err != nil {
+    fmt.Println("postUserMessage ERROR: ", err)
+  }
+
+  // for v2 only
+  authHeader := fmt.Sprintf("Bearer %s", token)
+  req.Header.Add("Authorization", authHeader)
+  req.Header.Add("Accept", "application/json")
+  req.Header.Add("Content-Type", "application/json")
+
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    fmt.Println("client do ERROR: ", err)
+  }
+
+  defer resp.Body.Close()
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Read body ERROR: ", err)
+  }
+  fmt.Println(body)
+  return resp.Status, nil
 }
 
 
 func getContent(path string) ([]byte, error) {
-	req, err := http.NewRequest("GET", path, nil)
-	if err != nil {
-		fmt.Println("getContent ERROR: ", err)
-	}
+  req, err := http.NewRequest("GET", path, nil)
+  if err != nil {
+    fmt.Println("getContent ERROR: ", err)
+  }
 
-        // for v2 only
-        //authHeader := fmt.Sprintf("Bearer %s", token)
-        //req.Header.Add("Authentication", authHeader)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("client do ERROR: ", err)
-	}
+  // for v2 only
+  //authHeader := fmt.Sprintf("Bearer %s", token)
+  //req.Header.Add("Authentication", authHeader)
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    fmt.Println("client do ERROR: ", err)
+  }
 
-	defer resp.Body.Close()
+  defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Read body ERROR: ", err)
-	}
-	return body, nil
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println("Read body ERROR: ", err)
+  }
+  return body, nil
 }
 
 func (h Hipchat) ListRooms() (error) {
-	var hf HipchatFeed
-	queryParams := fmt.Sprintf("auth_token=%s", h.token)
-	url := h.url + "/rooms/list?" + queryParams
-	content, err := getContent(url)
-	err = json.Unmarshal(content, &hf)
-	for _, item := range hf.Rooms {
-          fmt.Printf("Name: %v Topic: %v\n", item.Name, item.Topic)
-	}
-	if err != nil {
-		return err
-	}
-	return nil
+  var hf HipchatFeed
+  queryParams := fmt.Sprintf("auth_token=%s", h.token)
+  url := h.url + "/rooms/list?" + queryParams
+  content, err := getContent(url)
+  err = json.Unmarshal(content, &hf)
+  for _, item := range hf.Rooms {
+    fmt.Printf("Name: %v Topic: %v\n", item.Name, item.Topic)
+  }
+  if err != nil {
+    return err
+  }
+  return nil
 }
 
 func (h Hipchat) MessageRoom(room,message,from string) (string, error) {
-	url := fmt.Sprintf("%s/room/%s/notification", h.url, room)
-	content, err := postMessage(url, room, message, h.token)
-        return content, err
+  url := fmt.Sprintf("%s/room/%s/notification", h.url, room)
+  content, err := postMessage(url, room, message, h.token)
+  return content, err
+}
+
+func (h Hipchat) MessageUser(user, message, from string) (string, error) {
+  fmt.Println("i am in func")
+  url := fmt.Sprintf("%s/user/%s/message", h.url, user)
+  content, err := postUserMessage(url, user, message, h.token)
+  return content, err
 }
